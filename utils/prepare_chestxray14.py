b00c8a88e5cf4c2b07d95e62d2e31238b1001549
mewoodland <mewoodland@mdanderson.org>
2023-11-25 20:37:01 -0600

    chestxray processing

---
+++ utils/prepare_chestxray14.py
@@ -0,0 +1,27 @@
+"""
+Convert all PNG images in a folder to grayscale.
+"""
+
+# Imports
+import argparse
+import os
+import tqdm
+from PIL import Image
+
+# Arguments
+parser = argparse.ArgumentParser()
+parser._action_groups.pop()
+required = parser.add_argument_group('Required Arguments')
+required.add_argument('--in_dir', required=True, type=str, help='Path to folder containing the PNGs to convert to grayscale')
+required.add_argument('--out_dir', required=True, type=str, help='Path to folder to put grayscale PNGs into')
+args = parser.parse_args()
+in_dir = args.in_dir
+out_dir = args.out_dir
+if not os.path.exists(out_dir):
+  os.mkdir(out_dir)
+
+# Main code
+for fname in tqdm.tqdm(os.listdir(in_dir)):
+  image = Image.open(os.path.join(in_dir, fname))
+  image = image.convert('L')
+  image.save(os.path.join(out_dir, fname))
