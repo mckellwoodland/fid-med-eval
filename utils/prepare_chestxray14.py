d3cd48a98b50edac72ad46d7bc06f0310fd81a1a
McKell Woodland <mckell.woodland@gmail.com>
2023-11-25 22:09:24 -0500

    Update docstring

--- utils/prepare_chestxray14.py
+++ utils/prepare_chestxray14.py
@@ -13,8 +13,8 @@ from PIL import Image
 parser = argparse.ArgumentParser()
 parser._action_groups.pop()
 required = parser.add_argument_group('Required Arguments')
-required.add_argument('--in_dir', required=True, type=str, help='Path to folder containing the PNGs to convert to grayscale')
-required.add_argument('--out_dir', required=True, type=str, help='Path to folder to put grayscale PNGs into')
+required.add_argument('--in_dir', required=True, type=str, help='Path to folder containing original PNGs.')
+required.add_argument('--out_dir', required=True, type=str, help='Path to folder to put processed PNGs into.')
 args = parser.parse_args()
 in_dir = args.in_dir
 out_dir = args.out_dir
